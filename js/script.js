72f131b111690482c381b4c3ff299822c630b890
JonathanRoz <rozarioarnov@gmail.com>
2022-02-09 11:20:34 +0600

    responsive 3 div

--- js/script.js
+++ js/script.js
@@ -10,6 +10,44 @@ $(".item-slider").slick({
     dots:true,
     arrows:false,
     slidesToShow:3,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
 
 
 })
